00d7aeb7133da9c85acea4be903c87c2045a87b3
willwips <90972812+willwips@users.noreply.github.com>
2023-02-18 09:58:04 +0100

    test animation perso

--- graphic_main.py
+++ graphic_main.py
@@ -7,6 +7,7 @@ screen = None
 frame = None
 
 
+
 def initialisation(full_screen=False):
     pygame.init()
     global screen
@@ -25,16 +26,21 @@ def boucle():
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
-        global frame
-        global current
-        global screen
-        screen.fill((0, 0, 0))
-        screen.blit(frame[current], frame[current].get_rec())
-        time.sleep(0.01)
+    global frame
+    global current
+    global screen
+    screen.fill((0, 0, 0))
+    screen.blit(frame[current], frame[current].get_rect())
+    time.sleep(0.1)
+    pygame.display.flip()
+    current += 1
+    current = current%4
+
 
 def import_character_picture():
-    player1 = pygame.image.load('..\picture\character\player1.png').convert()
-    player2 = pygame.image.load('../picture/character/player2.png').convert()
-    player3 = pygame.image.load('../picture/character/player3.png').convert()
-    frame = [player1, player2, player3]
+    player1 = pygame.image.load('picture/character/player1.png').convert()
+    player2 = pygame.image.load('picture/character/player2.png').convert()
+    player3 = pygame.image.load('picture/character/player3.png').convert()
+    global frame
+    frame = [player1, player2, player1, player3]
     return frame
