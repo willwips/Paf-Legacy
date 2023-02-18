fbeeb32138dc91ac8b5d947bac7ce84092f237cf
willwips <90972812+willwips@users.noreply.github.com>
2023-02-18 09:44:15 +0100

    ajout player + code qui marche pas

--- graphic_main.py
+++ graphic_main.py
@@ -1,6 +1,10 @@
+import time
+
 import pygame
+import sys
 
 screen = None
+frame = None
 
 
 def initialisation(full_screen=False):
@@ -9,6 +13,28 @@ def initialisation(full_screen=False):
     if full_screen:
         screen = pygame.display.set_mode((300, 200), flags=pygame.FULLSCREEN)
     else:
-        screen = pygame.display.set_mode((300, 200), flags=pygame.FULLSCREEN)
+        screen = pygame.display.set_mode((300, 200))
 
     return screen
+
+
+current = 0
+
+
+def boucle():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            sys.exit()
+        global frame
+        global current
+        global screen
+        screen.fill((0, 0, 0))
+        screen.blit(frame[current], frame[current].get_rec())
+        time.sleep(0.01)
+
+def import_character_picture():
+    player1 = pygame.image.load('..\picture\character\player1.png').convert()
+    player2 = pygame.image.load('../picture/character/player2.png').convert()
+    player3 = pygame.image.load('../picture/character/player3.png').convert()
+    frame = [player1, player2, player3]
+    return frame
---
+++ main.py
@@ -0,0 +1,7 @@
+import graphic_main
+
+graphic_main.initialisation()
+graphic_main.import_character_picture()
+
+while True:
+    graphic_main.boucle()
