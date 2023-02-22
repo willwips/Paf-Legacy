c653f7660215893722873daccc1bd8667f1c1d81
willwips <90972812+willwips@users.noreply.github.com>
2023-02-22 18:06:46 +0100

    GAMEPLAY FOU

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ import weapon
 screen = graphic_main.initialisation()
 frame = graphic_main.import_character_picture(6)
 ennemy.spawn_enemy([50, 50], 'picture/character/player1.png', 50)
-ennemy.spawn_enemy([100, 100], 'picture/character/player1.png', 50)
+
+for i in range(0, 600, 50):
+    for j in range(0, 600, 50):
+        ennemy.spawn_enemy([i, j], 'picture/character/player1.png', 50)
 
 weapon.import_weapon()
 
