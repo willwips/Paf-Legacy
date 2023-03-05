70cd933081c403360b0ef877eee12d7d90e5ac44
willwips <90972812+willwips@users.noreply.github.com>
2023-03-05 11:00:42 +0100

    est ce possible?

--- main.py
+++ main.py
@@ -11,9 +11,9 @@ frame = graphic_main.import_character_picture(6)
 ennemy.spawn_enemy_1_2([50, 50], ['picture/enemy/bat/Bat_1.png', 'picture/enemy/bat/Bat_2.png'], 50)
 
 
-#for i in range(1, 10):
-#    for j in range(1, 10):
-#        ennemy.spawn_enemy_1_2([i*50, j*50], 'picture/enemy/bat/Bat_1.png', 50)
+for i in range(1, 10):
+    for j in range(1, 10):
+        ennemy.spawn_enemy_1_2([i*50, j*50], ['picture/enemy/bat/Bat_1.png', 'picture/enemy/bat/Bat_2.png'], 10)
 
 weapon.import_weapon()
 graphic_main.r, graphic_main.u = room.room_1()
