7a218fccb9c1c44b4bbd4e351e7f6e21d983a6eb
WagganImp <125686777+WagganImp@users.noreply.github.com>
2023-04-05 13:33:02 +0200

    push my boy

--- world.py
+++ world.py
@@ -10,7 +10,7 @@ def create_floor(_level):
     global floor
     if _level == 0:
         first_room = room.room_1_0
-        liste_level = [room.room_1_1, room.room_1_2, room.room_1_3, room.room_1_4, room.room_1_5, room.room_1_6, room.room_1_7, room.room_1_8, room.room_1_9]
+        liste_level = [room.room_1_1, room.room_1_2, room.room_1_3, room.room_1_4, room.room_1_5, room.room_1_6, room.room_1_7, room.room_1_8, room.room_1_9, room.room_1_10]
         boss_room = room.room_boss_1
         pos_boss = [9, random.randint(0, 9)]
     for i in range(10):
