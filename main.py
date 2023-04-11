4d9ec0c5ee42cbc6b1d03e55fa849ec265ecbc4c
WagganImp <125686777+WagganImp@users.noreply.github.com>
2023-04-11 18:17:37 +0200

    room 5 - 9

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ frame = graphic_main.import_character_picture(6)
 ennemy.spawn_enemy_1_2([50, 50], ['picture/enemy/bat/Bat_1.png', 'pictureussssss/enemy/bat/Bat_2.png'], 50)
 ennemy.spawn_enemy_1_2([250, 150], ['picture/enemy/bat/Bat_1.png', 'picture/enemy/bat/Bat_2.png'], 50)
 sennemy.spawn_enemy_2_1([350, 350],  ['picture/enemy/foe/foe_L_1.png', 'picture/enemy/foe/foe_L_2.png', 'picture/enemy/foe/foe_L_3.png', 'picture/enemy/foe/foe_R_1.png', 'picture/enemy/foe/foe_R_2.png', 'picture/enemy/foe/foe_R_3.png'], 50)
-ennemy.spawn_enemy_1([21, 223], 'picture/characteru/player1.png', 50)
+ennemy.spawn_enemy_1([21, 223], 'picture/character/player1.png', 50)
 ennemy.spawn_enemy_1([49, 27], 'picture/character/player1.png', 50)
 
 ennemy.spawn_enemy_1([x, y], 'picture/character/player1.png', 50)
