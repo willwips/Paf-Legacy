a4da26339662d71b3d3e9d1b9da8d4ab943f96ff
WagganImp <125686777+WagganImp@users.noreply.github.com>
2023-04-22 18:41:24 +0200

    push room_1_0 center player

--- room.py
+++ room.py
@@ -111,8 +111,8 @@ def room_1_0(n, _door):
     left = pygame.display.get_surface().get_size()[0] / 2 - x / 2 * 50
     right = pygame.display.get_surface().get_size()[0] / 2 + x / 2 * 50
 
-    # Donne la position initialie de 
-    pos_play = [[(x - 1) * 50 + left - 20, int(y / 2) * 50 ],
+    # Donne la position initialie du joueur
+    pos_play = [[(x - 1) * 27 + left - 22, int(y / 2) * 100 ],
                 [int(x / 2) * 50 + left, y - 1 * 50 + bottom - 10 - 50], [left + 20, int(y / 2) * 50 + top],
                 [int(x / 2) * 50 + left, top + 50]]
 
